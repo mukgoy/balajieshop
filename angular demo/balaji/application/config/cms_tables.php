040d391229749aebb6c29bb5392b19b7be7db0c5
mukgoy <m.goyal1988@gmail.com>
2019-08-06 11:43:45 +0530

    delete balaji

--- angular demo/balaji/application/config/cms_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$config['cms_posts'] = 'cms_posts';
-$config['cms_categories'] = 'cms_categories';
-$config['cms_category_meta_data'] = 'cms_category_meta_data';
-
-
--- angular demo/balaji/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-// http://marcoslin.github.io/angularAMD/#/home
-
-require.config({
-    baseUrl: 'app',
-    urlArgs: 'v=1.0'
-});
